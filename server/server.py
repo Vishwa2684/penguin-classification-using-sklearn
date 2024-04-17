2ea020a6526f09b06ed4fb3302dd95d36a3e4a02
Vishwa <vishwateja2684@gmail.com>
2024-04-17 10:17:49 +0530

    done

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 import requests
 
-model = pickle.load(open('../model.pkl'))
+model = pickle.load(open('./model.pkl'))
 
 app=Flask(__name__)
 CORS(app)
